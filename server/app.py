10c3151b4ca4843625e94da08104f75a455f9a7a
AnitaNamjoon <anitachepkemoi1@gmail.com>
2023-09-28 21:16:51 +0300

    New commit

--- server/app.py
+++ server/app.py
@@ -18,40 +18,78 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
 
+
+class ClearSession(Resource):
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
 class ShowArticle(Resource):
-
     def get(self, id):
         session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
         session['page_views'] += 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
             article_json = jsonify(article.to_dict())
-
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
+
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+       
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+           
+            session['user_id'] = user.id
+            response_data = user.to_dict() 
+            response = jsonify(response_data)
+            response.headers['Content-Type'] = 'application/json' 
+            return response, 200
+        else:
+            return {'message': 'User not found'}, 404
+
+class Logout(Resource):
+    def delete(self):
+       
+        session.pop('user_id', None)
+        return {}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            user_data = user.to_dict()  
+            response = jsonify(user_data)
+            response.headers['Content-Type'] = 'application/json'  
+            return response, 200
+        else:
+            return {}, 401
+
+
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
+
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
